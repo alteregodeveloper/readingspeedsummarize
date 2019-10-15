2638c129daffa740e7ad252cf5dedb313556a29d
alterego developer <alteregodeveloper@outlook.com>
2019-10-15 14:25:54 -0500

    Lang file is added

---
+++ lang/en/block_readingspeedsummarize.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @package   block_readingspeedsummarize
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Reading Speed Summarize';
+$string['readingspeedsummarize'] = 'Reading Speed Summarize';
+$string['modulenameplural'] = 'Reading Speed Summarizes';
