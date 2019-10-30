6ac6a224e32f50071ca63411a0f0448cdb1c7445
alterego developer <alteregodeveloper@outlook.com>
2019-10-30 17:51:38 -0600

    Spanish language files are added

---
+++ lang/es/block_readingspeedsummarize.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @package   block_readingspeedsummarize
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Seguimiento de Velocidad Lectora';
+$string['readingspeedsummarize'] = 'Seguimiento de Velocidad Lectora';
+$string['modulenameplural'] = 'Seguimiento de Velocidad Lectora';
+$string['results'] = 'Resultados';
